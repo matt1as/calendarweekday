a839384c58c807c63d17fd70541187f546955a97
Mattias Johansson <mattias.e.johansson@gmail.com>
2024-09-01 20:14:45 +0200

    Add year input field

--- src/App.js
+++ src/App.js
@@ -7,22 +7,25 @@ import './App.css';
 function App() {
   const [date, setDate] = useState(new Date());
   const [weekNumber, setWeekNumber] = useState(getWeek(new Date(), { weekStartsOn: 1 }));
+  const [year, setYear] = useState(getYear(new Date()));
   const [startDate, setStartDate] = useState('');
   const [endDate, setEndDate] = useState('');
 
   useEffect(() => {
-    updateDates(weekNumber);
+    updateDates(weekNumber, year);
   }, []);
 
   const onChange = (newDate) => {
     setDate(newDate);
     const weekNum = getWeek(newDate, { weekStartsOn: 1 });
+    const yearNum = getYear(newDate);
     setWeekNumber(weekNum);
-    updateDates(weekNum);
+    setYear(yearNum);
+    updateDates(weekNum, yearNum);
   };
 
-  const updateDates = (week) => {
-    const firstDayOfWeek = startOfWeek(new Date(date.getFullYear(), 0, (week - 1) * 7 + 1), { weekStartsOn: 1 });
+  const updateDates = (week, yearNum) => {
+    const firstDayOfWeek = startOfWeek(new Date(yearNum, 0, (week - 1) * 7 + 1), { weekStartsOn: 1 });
     const lastDayOfWeek = endOfWeek(new Date(firstDayOfWeek.getTime() + 6 * 24 * 60 * 60 * 1000), { weekStartsOn: 1 });
 
     setStartDate(getDate(firstDayOfWeek) + '/' + (getMonth(firstDayOfWeek) + 1) + '/' + getYear(firstDayOfWeek));
@@ -38,8 +41,21 @@ function App() {
       return;
     }
     setWeekNumber(newWeekNumber);
-    updateDates(newWeekNumber);
-    setDate(new Date(date.getFullYear(), 0, (newWeekNumber - 1) * 7 + 1));
+    updateDates(newWeekNumber, year);
+    setDate(new Date(year, 0, (newWeekNumber - 1) * 7 + 1));
+  };
+
+  const handleYearChange = (e) => {
+    const newYear = parseInt(e.target.value, 10);
+    if (isNaN(newYear) || newYear < 1 || newYear > 9999) {
+      setYear('');
+      setStartDate('');
+      setEndDate('');
+      return;
+    }
+    setYear(newYear);
+    updateDates(weekNumber, newYear);
+    setDate(new Date(newYear, 0, (weekNumber - 1) * 7 + 1));
   };
 
   return (
@@ -54,11 +70,16 @@ function App() {
             value={weekNumber}
             onChange={handleWeekNumberChange}
           />
+          <input
+            type="number"
+            value={year}
+            onChange={handleYearChange}
+          />
         </div>
       </div>
       {startDate && endDate && (
         <p>
-          The dates for week {weekNumber} are {startDate} to {endDate}.
+          The dates for week {weekNumber} of {year} are {startDate} to {endDate}.
         </p>
       )}
     </div>
